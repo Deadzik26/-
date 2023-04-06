9777e0a0e4ab4a516849eb359165c1ef2da3c853
Deadzik26 <130091606+Deadzik26@users.noreply.github.com>
2023-04-06 21:53:51 +0300

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+class Triangle {
+private:
+    double a, b, c;
+
+public:
+    Triangle() : a(0), b(0), c(0) {}
+
+    Triangle(double sideA, double sideB, double sideC) : a(sideA), b(sideB), c(sideC) {}
+
+    double getSideA() const { return a; }
+    double getSideB() const { return b; }
+    double getSideC() const { return c; }
+
+    void setSideA(double side) { a = side; }
+    void setSideB(double side) { b = side; }
+    void setSideC(double side) { c = side; }
+
+    double getArea() const {
+        double p = (a + b + c) / 2;
+        return sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    void print_sqr() const {
+        std::cout << "Side A: " << a << std::endl;
+        std::cout << "Side B: " << b << std::endl;
+        std::cout << "Side C: " << c << std::endl;
+        std::cout << "Area: " << getArea() << std::endl;
+    }
+};
+
+int main() {
+    Triangle t(6, 8, 10);
+    t.print_sqr();
+    return 0;
+}
