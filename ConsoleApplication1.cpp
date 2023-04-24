0227f661d973bbf303479d731a546deb82d5f313
Deadzik26 <130091606+Deadzik26@users.noreply.github.com>
2023-04-25 01:00:29 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,62 +1,74 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
-class Hexagon {
-private:
-	float sidea;
+class Person {
+protected:
+    string name;
 public:
-	Hexagon() {
-		this->sidea = 0;
-	}
-
-	Hexagon(float sidea) {
-		this->sidea = sidea;
-	}
-
-	~Hexagon() {
-		cout << "Constructor destroyed" << endl;
-	}
-
-	void setsidea(float sidea) {
-		this->sidea = sidea;
-	}
-
-	float getsidea() {
-		return this->sidea;
-	}
-
-	float getarea() {
-		return ((3 * sqrt(3)) / 2) * pow(this->sidea, 2);
-	}
-
-	friend Hexagon frd(Hexagon a, Hexagon b);
+    Person() {
+        name = "Unknown";
+    }
+    Person(string n) {
+        name = n;
+    }
+    virtual ~Person() {}
+    string getName() const { return name; }
 };
 
-void print_sqrt(Hexagon S) {
-	cout << "Sidea: " << S.getsidea() << " area: " << S.getarea() << endl;
-}
+class Employee : public Person {
+protected:
+    double salary;
+public:
+    Employee() : Person() {
+        salary = 0;
+    }
+    Employee(string n, double s) : Person(n) {
+        salary = s;
+    }
+    virtual ~Employee() {}
+    double getSalary() const { return salary; }
+};
 
-Hexagon frd(Hexagon a, Hexagon b) {
-	float new_sidea = sqrt((a.getarea() + b.getarea()) * 2 / (3 * sqrt(3)));
-	Hexagon c = Hexagon(new_sidea);
-	return c;
-}
+class Worker : public Employee {
+protected:
+    int hours;
+public:
+    Worker() : Employee() {
+        hours = 0;
+    }
+    Worker(string n, double s, int h) : Employee(n, s) {
+        hours = h;
+    }
+    virtual ~Worker() {}
+    int getHours() const { return hours; }
+};
+
+class Engineer : public Employee {
+protected:
+    string specialty;
+public:
+    Engineer() : Employee() {
+        specialty = "Unknown";
+    }
+    Engineer(string n, double s, string sp) : Employee(n, s) {
+        specialty = sp;
+    }
+    virtual ~Engineer() {}
+    string getSpecialty() const { return specialty; }
+};
 
 int main() {
-	float sidea;
-	cout << "Write sidea of Hexagon 1: ";
-	cin >> sidea;
-	Hexagon a = Hexagon(sidea);
+    Person p("John Smith");
+    Employee e("Jane Doe", 50000);
+    Worker w("Bob Johnson", 30000, 40);
+    Engineer en("Alice Lee", 75000, "Software Engineering");
 
-	cout << "Write sidea of Hexagon 2: ";
-	cin >> sidea;
-	Hexagon b = Hexagon(sidea);
+    cout << "Person name: " << p.getName() << endl;
+    cout << "Employee name: " << e.getName() << ", salary: " << e.getSalary() << endl;
+    cout << "Worker name: " << w.getName() << ", salary: " << w.getSalary() << ", hours: " << w.getHours() << endl;
+    cout << "Engineer name: " << en.getName() << ", salary: " << en.getSalary() << ", specialty: " << en.getSpecialty() << endl;
 
-	print_sqrt(a);
-	print_sqrt(b);
-
-	Hexagon c = frd(a, b);
-	print_sqrt(c);
+    return 0;
 }
